89ad7ed4b04a3b2288d81b23f512f3f5d8b7c75c
Alexander Kalashnikov <a.kalashnikov@pravo.tech>
2023-06-14 09:31:20 +0400

    Add negative tests

---
+++ tests/WaitFinish.Tests/NegativeTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace WaitFinish.Tests
+{
+	[TestFixture]
+	public class NegativeTests
+	{
+		[Test]
+		public void NegativeTest()
+		{
+			Assert.AreEqual(true, false);
+		}
+	}
+}
